c94031869a3d57515b36d23d4c393e6ff3f12334
Your Name <you@example.com>
2021-07-14 17:17:00 +0200

    few changes

--- src/main/webapp/wikipediaApi.js
+++ src/main/webapp/wikipediaApi.js
@@ -1,17 +1,16 @@
 async function wiki() {
-  await fetch(
-    `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exchars=1000&titles=Pizza`
-  )
-    .then((response) => response.json())
-    .then((data) =>
-      addData(data.query.pages[Object.keys(data.query.pages)[0]].extract)
-    );
+    await fetch(
+            `https://api.codetabs.com/v1/proxy/?quest=https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exchars=1000&titles=Pizza`
+        )
+        .then((response) => response.json())
+        .then((data) =>
+            addData(data.query.pages[Object.keys(data.query.pages)[0]].extract)
+        );
 }
 
 function addData(data) {
-  const element = document.getElementById("foodhistory");
-  var node = document.createElement("p"); 
-  node.innerHTML = data;
-  element.appendChild(node);
+    const element = document.getElementById("foodhistory");
+    var node = document.createElement("p");
+    node.innerHTML = data;
+    element.appendChild(node);
 }
-
