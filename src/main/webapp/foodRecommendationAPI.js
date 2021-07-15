1d1af4a4f8b859632ee17d28ea31f96fb19ae37a
Your Name <you@example.com>
2021-07-15 13:51:23 +0200

    made few edits

--- src/main/webapp/foodRecommendationAPI.js
+++ src/main/webapp/foodRecommendationAPI.js
@@ -8,23 +8,23 @@ async function initDB() {
     const categories = ["Beef", "Chicken", "Goat", "Lamb", "Pasta", "Pork", "Seafood", "Vegetarian", "Dessert", "Breakfast", "Miscellaneous", "Vegan"];
     const temp = {}
 
-    for(var c of categories) {
-        const response = await fetch('https://themealdb.com/api/json/v1/1/filter.php?c='+ c)
+    for (var c of categories) {
+        const response = await fetch('https://themealdb.com/api/json/v1/1/filter.php?c=' + c)
         const foods = await response.json()
-        
-        for(let i = 0; i < foods.meals.length; i++){
-            if(c in temp === false) {
+
+        for (let i = 0; i < foods.meals.length; i++) {
+            if (c in temp === false) {
                 temp[c] = [foods.meals[i]]
-            }
-            else {
+            } else {
                 temp[c].push(foods.meals[i])
             }
         }
     }
+
     return temp
 }
 
-(async () => {
+(async() => {
     MealDB = await initDB()
 })();
 
@@ -35,49 +35,50 @@ function generateRecommendation() {
     var list = {};
 
     // Step 1: Get Meal Type and store chosen categories in an array        
-    if(document.getElementById('breakfastMeal').checked) {
+    if (document.getElementById('breakfastMeal').checked) {
         temp.push("Breakfast");
     }
-    if(document.getElementById('lunchDinnerMeal').checked) {
+    if (document.getElementById('lunchDinnerMeal').checked) {
         temp.push(getPreferences())
     }
-    if(document.getElementById('dessertMeal').checked) {
+    if (document.getElementById('dessertMeal').checked) {
         temp.push("Dessert");
     }
 
     // Creates a 1D array
-    for(let i = 0; i< temp.length; i++) {
+    for (let i = 0; i < temp.length; i++) {
         options = options.concat(temp[i]);
     }
+    console.log(options)
 
     // Step 2: Get Allergies and Filter out the allergies
-    $( "#allergy-select")
-    .change(function() {
-        tags = "";
-        $( "select option:selected" ).each(function() {
-            tags += "&health=" + $( this ).val();
-        });
-    })
-    .trigger( "change" );
+    $("#allergy-select")
+        .change(function() {
+            tags = "";
+            $("select option:selected").each(function() {
+                tags += "&health=" + $(this).val();
+            });
+        })
+        .trigger("change");
 
     // Step 3: Convert all foods in array to an Edamam Object
-    convertToEdamamObjects(options, tags)
+    MealDB != {} ? convertToEdamamObjects(options, tags) : null
 }
 
 function getPreferences() {
     var pref_list = [];
 
-    if(document.getElementById('prefMeat').checked) {
+    if (document.getElementById('prefMeat').checked) {
         // TODO: Separate all meats
-        insert(pref_list, "Beef","Chicken","Goat","Lamb","Pork");
+        insert(pref_list, "Beef", "Chicken", "Goat", "Lamb", "Pork");
     }
-    if(document.getElementById('prefSeafood').checked) {
+    if (document.getElementById('prefSeafood').checked) {
         pref_list.push("Seafood");
     }
-    if(document.getElementById('prefPasta').checked) {
+    if (document.getElementById('prefPasta').checked) {
         pref_list.push("Pasta");
     }
-    if(document.getElementById('prefVeggie').checked) {
+    if (document.getElementById('prefVeggie').checked) {
         // TODO: Separate Vegetarian and Vegan items
         pref_list.push.apply(pref_list, ["Vegetarian", "Vegan"]);
     }
@@ -88,28 +89,26 @@ function getPreferences() {
 // FIXME: Edamam API confuses words like 'and' and 'with' when converting the MealDB API objects
 async function convertToEdamamObjects(options, tags) {
     const temp = {}
-    
-    for (var c of options){
-        for(let i = 0; i < MealDB[c].length; i++){
+
+    for (var c of options) {
+        for (let i = 0; i < MealDB[c].length; i++) {
             const foodName = MealDB[c][i].strMeal;
             const query = foodName.replace(' ', "%20")
             var response = null;
 
             // TODO: Add Postal and Long and Lat fields to fetch request to better the search          
-            if("Vegan" in options || "Vegetarian" in options) {
-                response = await fetch("https://api.edamam.com/api/menu-items/v2/search?app_id=2ba0ec45&app_key=ac665ae3e41a33e09736700ec5a0c738&q="+foodName+"&health=vegan&health=vegetarian" + tags)
-            }
-            else{
-                response = await fetch("https://api.edamam.com/api/menu-items/v2/search?app_id=2ba0ec45&app_key=ac665ae3e41a33e09736700ec5a0c738&q="+foodName + tags)
+            if ("Vegan" in options || "Vegetarian" in options) {
+                response = await fetch("https://api.edamam.com/api/menu-items/v2/search?app_id=2ba0ec45&app_key=ac665ae3e41a33e09736700ec5a0c738&q=" + foodName + "&health=vegan&health=vegetarian" + tags)
+            } else {
+                response = await fetch("https://api.edamam.com/api/menu-items/v2/search?app_id=2ba0ec45&app_key=ac665ae3e41a33e09736700ec5a0c738&q=" + foodName + tags)
             }
 
             const results = await response.json();
-            
-            for(let i = 0; i < results.hints.length; i ++) {
-                if(foodName in temp === false) {
+
+            for (let i = 0; i < results.hints.length; i++) {
+                if (foodName in temp === false) {
                     temp[foodName] = [results.hints[i]]
-                }
-                else {
+                } else {
                     temp[foodName].push(results.hints[i])
                 }
             }
@@ -121,34 +120,35 @@ async function convertToEdamamObjects(options, tags) {
 
 // Step 4: Randomly pick 3 food options and display to recommendation cards
 // TODO: Add timer delay to make transition smoother when random item suggestion is clicked again
-function postRecommendations(options, recommendations){
+function postRecommendations(options, recommendations) {
     // TODO: Clean up Recommendation loops; too slow
-    for(let i = 0; i < recommendations.length; i++) {
+    for (let i = 0; i < recommendations.length; i++) {
         var recMeal = recommendations[i].key;
         var recMealThumb = "";
         var isFound = false;
 
-        for(var e of options) {
-            for(let i = 0; i < MealDB[e].length; i++){
-                if(MealDB[e][i].strMeal === recMeal) {
+        for (var e of options) {
+            for (let i = 0; i < MealDB[e].length; i++) {
+                if (MealDB[e][i].strMeal === recMeal) {
                     recMealThumb = MealDB[e][i].strMealThumb;
                     isFound = true;
                     break;
                 }
             }
-            if(isFound === true)
+            if (isFound === true)
                 break;
         }
-        
+
         $("#rec-card-" + i + " img").attr("src", recMealThumb)
         $("#rec-card-" + i + " div h5").text(recMeal)
+        $("#rec-card-" + i + " a").attr("href", `./result.html?food=${recMeal}`)
     }
 }
 
 function randomRec(dict, iter) {
     var temp = [];
 
-    while(temp.length < iter){
+    while (temp.length < iter) {
         const index = Math.floor(Math.random() * Object.keys(dict).length)
         const key = Object.keys(dict)[index]
 
@@ -156,7 +156,7 @@ function randomRec(dict, iter) {
             key: key,
             value: dict[key]
         })
-        
+
         delete dict[key]
     }
     return temp;
